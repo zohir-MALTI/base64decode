276204a77fabd025dc3a5fab529acd63db558312
zohir-MALTI <57535055+zohir-MALTI@users.noreply.github.com>
2020-01-20 15:48:22 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,12 @@ const UPPERCASE_OFFSET: i8 = -65;
 const LOWERCASE_OFFSET: i8 = 26 - 97;
 const NUM_OFFSET: i8 = 52 - 48;
 
+// TGEgcmFjbGV0dGUgKEJyYXRjaMOkcywgwqsgZnJvbWFnZSDCuyByw7R0aSwgZW4gc3Vpc3NlIGFsbGVtYW5kKSBlc3QgZCd1bmUgcGFydCB1biBmcm9tYWdlIChsZSBvdSBsYSByYWNsZXR0ZSkgb3JpZ2luYWlyZSBkdSBjYW50b24gZHUgVmFsYWlzIGVuIFN1aXNzZSwgZXQgZCdhdXRyZSBwYXJ0LCB1bmUgcmVjZXR0ZSBkZSBjdWlzaW5lIHRyYWRpdGlvbm5lbGxlIGV0IGVtYmzDqW1hdGlxdWUgZGUgbGEgY3Vpc2luZSBzdWlzc2UsIGNvbm51ZSBkYW5zIGxlIG1vbmRlIGVudGllciwgdmFyaWFudGUgZGVzIGZvbmR1ZXMgYXUgZnJvbWFnZSwgw6AgYmFzZSBkZSBjZSBmcm9tYWdlIGZvbmR1LCByYWNsw6kgYXUgZnVyIGV0IMOgIG1lc3VyZSBxdeKAmWlsIGZvbmQsIGV0IHNlcnZpZSB0cmFkaXRpb25uZWxsZW1lbnQgYXZlYyBkZXMgcG9tbWVzIGRlIHRlcnJlIGVuIHJvYmUgZGVzIGNoYW1wcyBldCBhY2NvbXBhZ27DqWUgZGUgbMOpZ3VtZXMgYXUgdmluYWlncmUgKGNvcm5pY2hvbnMsIG9pZ25vbnMpLg ==
+
+// La raclette (Bratchäs, « fromage » rôti, en suisse allemand) est d'une part un fromage (le ou la raclette) 
+// originaire du canton du Valais en Suisse, et d'autre part, une recette de cuisine traditionnelle et emblématique de la cuisine suisse,
+// connue dans le monde entier, variante des fondues au fromage, à base de ce fromage fondu, raclé au fur et à mesure qu’il fond, et servie traditionnellement avec des pommes de terre en robe des champs et accompagnée de légumes au vinaigre (cornichons, oignons).
+
 fn base64decode(input: String) -> String {
     let result = input.trim().chars()
         .filter(|&ch| ch != '=')                                //Filter '=' chars
@@ -14,7 +20,8 @@ fn base64decode(input: String) -> String {
                 'a' ..= 'z' => ascii + LOWERCASE_OFFSET,
                 'A' ..= 'Z' => ascii + UPPERCASE_OFFSET,
                 '+' => 62,
-                '/' => 63
+                '/' => 63,
+                _   => panic!()     // panic pour arreter le programme (comme exit())
             };
             format!("{:#08b}", convert)[2..].to_string()        //convert indices to binary format and remove the two first digits
         })
@@ -33,7 +40,7 @@ fn base64decode(input: String) -> String {
 }
  
 fn main() {
-    let input = String::new();
+    let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     println!("Input: {}", input);
  
